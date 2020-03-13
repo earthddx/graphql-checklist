20136d8b5920e0aebe4748240266c39ac96cf30a
earthddx <47094239+earthddx@users.noreply.github.com>
2020-03-13 03:58:12 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+
+const client = new ApolloClient({
+  uri: "https://react-todo-graphql-exp.herokuapp.com/v1/graphql"
+});
+
+
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
